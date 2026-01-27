789ceaa162e84edd0abd660e2dbdcea83ecd3238
Inukaupek <cb012271@students.apiit.lk>
2026-01-28 00:23:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Retail Management System</h1>
+      <p>AWS Cloud Based Application</p>
+
+      <ul>
+        <li>User Login (Cognito)</li>
+        <li>Product Listing</li>
+        <li>Product Details</li>
+        <li>Order Placement</li>
+        <li>Stock Management</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
